d32c8cb7cd1bc189f68a81208f52f85702cdb5de
Terrance Chen <terrancechen2002@gmail.com>
2023-10-26 00:41:11 -0400

    Adding team.js with team and api work

--- front-end/src/Header.js
+++ front-end/src/Header.js
@@ -1,21 +1,21 @@
+// Header.js
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
-const Home = props => {
+import './Header.css';
+
+const Header = props => {
     return (
-        <div className="Home">
-            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <div className="Header">
+            <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
                 <h1>BBall Player Tracker</h1>
-            </Link>
+            </a>
             <nav>
                 <a href="/players">Players</a>
                 <a href="/teams">Teams</a>
                 <a href="/games">Games</a>
                 <a href="/favorites" className="active">Favorites</a>
             </nav>
-
-
         </div>
     );
 }
-export default Home
+
+export default Header;
