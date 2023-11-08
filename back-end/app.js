b73ac38a114ae9580505c4319ff79ca22202c657
Erikccc <lz2348@nyu.edu>
2023-11-08 15:56:53 -0500

    make change

--- back-end/app.js
+++ back-end/app.js
@@ -38,45 +38,6 @@ app.get("/", async (req, res) => {
         res.status(500).send(error.message);
     }
 });
-app.get('/favorites', async (req, res) => {
-    try {
-        // Fetch player stats from the API
-        const response = await axios.get('https://www.balldontlie.io/api/v1/stats', {
-            // Additional parameters can be added here to filter the stats as needed
-        });
-  
-        // Check if the API returned a list of player stats
-        if (response.data && response.data.data) {
-            const playerStats = response.data.data;
-            // Process and format the dates
-            const formattedPlayerStats = playerStats.map(player => {
-                const gameDate = new Date(player.game.date);
-                // Format the date to YYYY-MM-DD
-                const formattedDate = gameDate.toISOString().split('T')[0];
-                return {
-                    ...player,
-                    game: {
-                        ...player.game,
-                        date: formattedDate
-                    }
-                };
-            });
-  
-            // Shuffle the array and pick the first 10 items
-            const shuffledStats = formattedPlayerStats.sort(() => 0.5 - Math.random());
-            const selectedStats = shuffledStats.slice(0, 10);
-            
-            // Send the selected stats to the front end
-            res.json(selectedStats);
-        } else {
-            res.status(404).json({ message: 'No player stats found' });
-        }
-    } catch (error) {
-        console.error('Error fetching player stats: ', error);
-        res.status(500).json({ message: 'Error fetching player stats' });
-    }
-});
-
 
 
 
