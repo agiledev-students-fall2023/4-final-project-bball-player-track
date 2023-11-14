1c8e125365636f16f16e8556f8660020774741d4
michael-shu <michaelshu34@gmail.com>
2023-11-13 21:40:59 -0500

    add testing for games route

--- back-end/test/app.test.js
+++ back-end/test/app.test.js
@@ -5,6 +5,7 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
+/*
 describe('Login Route', function() {
 
     it('should return Welcome message for correct credentials', function(done) {
@@ -25,6 +26,21 @@ describe('Login Route', function() {
             .end(function(err, res) {
                 expect(res).to.have.status(401);
                 expect(res.body).to.have.property('message', 'Incorrect username or password');
+                done();
+            });
+    });
+});*/
+
+describe('Games Route', () => {
+    it('should return game data for correct route', function(done) {
+        chai.request(app)
+            .get('/games')
+            .end(function(err, res) {
+                expect(res).to.have.status(200);
+                expect(res.body[0]).to.have.property('id');
+                expect(res.body[0]).to.have.property('date');
+                expect(res.body[0]).to.have.property('home_team');
+                expect(res.body[0]).to.have.property('visitor_team');
                 done();
             });
     });
