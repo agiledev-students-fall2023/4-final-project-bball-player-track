1abdf3254eda3465a08b9d20e592da0834a0e1ed
jialinzhang010 <34475655+NKNKINU@users.noreply.github.com>
2023-11-23 01:07:41 -0500

    add localstorage

--- back-end/app.js
+++ back-end/app.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: `${process.env.REACT_APP_FRONTEND}`
 }));
 
 app.use("/static", express.static("public"));
