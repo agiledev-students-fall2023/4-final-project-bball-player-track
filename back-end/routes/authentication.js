6833e9a16f775f02e2af34a0735af358ad50bc9d
jialinzhang010 <34475655+NKNKINU@users.noreply.github.com>
2023-11-23 23:07:48 -0500

    add validator to auth

--- back-end/routes/authentication.js
+++ back-end/routes/authentication.js
@@ -2,53 +2,67 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("../models/User.js");
 
+const { body, validationResult } = require('express-validator');
+
 const authenticationRouter = () => {
   const router = express.Router();
 
-  router.post("/", async (req, res, next) => {
-    const { username, password } = req.body;
+  router.post("/",
+    body('username').isLength({ min: 4, max: 20 }),
+    body('password').isLength({ min: 4, max: 20 }),
+
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          success: false,
+          errors: errors.array(),
+          message: "Validation failed. Username and password must be between 4 and 20 characters."
+        });
+      }
+      const { username, password } = req.body;
 
 
-    try {
-      let user = await User.findOne({ username }).exec();
+      try {
+        let user = await User.findOne({ username }).exec();
 
-      if (user) {
-        if (user.validPassword(password)) {
-          console.log("User logged in successfully.");
+        if (user) {
+          if (user.validPassword(password)) {
+            console.log("User logged in successfully.");
+            const token = user.generateJWT();
+            res.json({
+              success: true,
+              message: "User logged in successfully.",
+              token,
+              username: user.username,
+            });
+          } else {
+            console.error("Incorrect password.");
+            res.status(401).json({
+              success: false,
+              message: "Incorrect password.",
+            });
+          }
+        } else {
+          user = await new User({ username, password }).save();
+          console.log(`New user created: ${user}`);
           const token = user.generateJWT();
-          res.json({
+          res.status(201).json({
             success: true,
-            message: "User logged in successfully.",
+            message: "New user created successfully.",
             token,
             username: user.username,
           });
-        } else {
-          console.error("Incorrect password.");
-          res.status(401).json({
-            success: false,
-            message: "Incorrect password.",
-          });
         }
-      } else {
-        user = await new User({ username, password }).save();
-        console.log(`New user created: ${user}`);
-        const token = user.generateJWT();
-        res.status(201).json({
-          success: true,
-          message: "New user created successfully.",
-          token,
-          username: user.username,
+      } catch (err) {
+        console.error(`Authentication failed: ${err}`);
+        res.status(500).json({
+          success: false,
+          message: "Error with user authentication.",
+          error: err,
         });
       }
-    } catch (err) {
-      console.error(`Authentication failed: ${err}`);
-      res.status(500).json({
-        success: false,
-        message: "Error with user authentication.",
-        error: err,
-      });
-    }
-  });
+    });
 
   return router;
 };
--- front-end/src/Auth.js
+++ front-end/src/Auth.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Auth.css';
 
+
 const Auth = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -19,14 +20,14 @@ const Auth = () => {
             });
 
             const data = await response.json();
-            
+
             if (response.ok) {
-                
+
                 localStorage.setItem('token', data.token);
                 localStorage.setItem('username', username);
-                
+
                 // window.location.href = process.env.REACT_APP_FRONTEND;
-                
+
                 window.location.href = 'http://localhost:3000';
 
             } else {
@@ -41,12 +42,15 @@ const Auth = () => {
         <div className="login-container">
             <form className="login-form" onSubmit={handleSubmit}>
                 Welcome to BBall Player Tracker!
+                <span className="help-icon" title="If you don't have an account, we will automatically register one for you.">?</span>
                 <input
                     className="login-input"
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     placeholder="Username"
+                    minLength="4"
+                    maxLength="20"
                     required
                 />
                 <input
@@ -55,6 +59,8 @@ const Auth = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     placeholder="Password"
+                    minLength="4"
+                    maxLength="20"
                     required
                 />
                 {error && <p className="error-message">{error}</p>}
