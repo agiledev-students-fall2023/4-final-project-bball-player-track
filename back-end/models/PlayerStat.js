6c70e72b3ff01a8e4ce9c5e49210fff423f522d5
“tchen0125” <terrancechen2002@gmail.com>
2023-11-27 02:51:05 -0500

    Update to Players now lists all players and stats with DB

---
+++ back-end/models/PlayerStat.js
@@ -0,0 +1,13 @@
+// models/PlayerStat.js
+const mongoose = require('mongoose');
+
+const playerStatSchema = new mongoose.Schema({
+  fullName: String,
+  ppg: Number,
+  apg: Number,
+  rpg: Number,
+  spg: Number,
+  lastUpdated: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('PlayerStat', playerStatSchema);
